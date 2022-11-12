47e60f42d8c4ef001db8935d75e26c3a311ae776
NicholasMoc <102491443+NicholasMoc@users.noreply.github.com>
2022-11-11 23:46:15 -0800

    Added a UI for rps, includes buttons to select choice

--- myscripts.js
+++ myscripts.js
@@ -1,86 +1,92 @@
+const buttons = document.querySelectorAll('button');
+
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.className, getComputerChoice());
+        if(p == 5)
+        {
+            results.textContent = "Congrats on beating the computer to 5 wins! You have successfully beat the computer!";
+            buttons.forEach((button) => {button.disabled = true;});
+        }
+        if(c == 5)
+        {
+            results.textContent = "Computer has reached 5 wins. Sorry, you have lost.";
+            buttons.forEach((button) => {button.disabled = true;});
+        }
+    });
+});
+p = 0;
+c = 0;
+
+const results = document.querySelector('.results');
+const pScore = document.querySelector('.playerScore');
+const cScore = document.querySelector('.compScore');
+cScore.textContent = c;
+pScore.textContent = p;
+
 function getComputerChoice() {
     let choicearr = ['rock', 'paper', 'scissors'];
     return choicearr[Math.floor(Math.random()*choicearr.length)];
 }
 
-function play(playerSelection, computerSelection) {
-    let pS = playerSelection.toLowerCase();
-
+function playRound(playerSelection, computerSelection) {
+    let pS = playerSelection;
     if (pS == computerSelection)
     {
-        return "Tie!";
+        results.textConten="Tie!";
     }
 
     if (pS == 'rock')
     {
         if(computerSelection == 'paper')
         {
-            return "You Lose! Paper beats Rock";
+            results.textContent = "You Lose! Paper beats Rock";
+            c++;
+            cScore.textContent = c.toString();
         }
         else
         {
-            return "You Win! Rock beats Scissors";
+            results.textContent="You Win! Rock beats Scissors";
+            p++;
+            pScore.textContent = p.toString();
         }
     }
     else if (pS == 'paper')
     {
         if(computerSelection == 'rock')
         {
-            return "You Win! Paper beats Rock";
+            results.textContent="You Win! Paper beats Rock";
+            p++;
+            pScore.textContent = p.toString();
         }
         else
         {
-            return "You Lose! Scissors beats Paper";
+            results.textContent="You Lose! Scissors beats Paper";
+            c++;
+            cScore.textContent = c.toString();
         }
     }
     else if (pS == 'scissors')
     {
         if(computerSelection == 'rock')
         {
-            return "You Lose! Rock beats Scissors";
+            results.textContent="You Lose! Rock beats Scissors";
+            c++;
+            cScore.textContent = c.toString();
         }
         else
         {
-            return "You Win! Scissors beats Paper";
+            results.textContent="You Win! Scissors beats Paper";
+            p++;
+            pScore.textContent = p.toString();
         }
     }
     else
     {
-        return "This is an invalid choice.";
-    }
-
-}
-
-function game()
-{
-    pScore = 0;
-    cScore = 0;
-    for (let i = 0; i < 5; i++)
-    {
-        pC = prompt("Enter Choice: ");
-        cC = getComputerChoice();
-        res = play(pC, cC)
-        console.log(res);
-        if (res.charAt(4) == 'W')
-        {
-            pScore++;
-        }
-        else if(res.charAt(4) == 'L')
-        {
-            cScore++;
-        }
-    }
-    if(pScore > cScore)
-    {
-        console.log("Final: Player Wins");
-    }
-    else if(cScore > pScore) {
-        console.log("Final: Computer Wins");
-    }
-    else
-    {
-        console.log("Final: Tie")
+        results.textContent="This is an invalid choice.";
     }
 }
+
 
 
